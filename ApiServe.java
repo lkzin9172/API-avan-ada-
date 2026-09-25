491edc70b5506bd524d39a00324b084519b13c8e
lkzin9172 <kaiqueisac6@gmail.com>
2026-09-25 18:45:55 -0300

    Add ApiServe.java file structure to README

---
+++ ApiServe.java
@@ -0,0 +1,10 @@
+/projeto-api-docker
+│
+├── Dockerfile             <-- (O arquivo de configuração do Docker)
+└── /src
+    └── /main
+        └── /java
+            └── /com
+                └── /freefire
+                    └── /api
+                        └── ApiServer.java  <-- (O código da API que te passei)
